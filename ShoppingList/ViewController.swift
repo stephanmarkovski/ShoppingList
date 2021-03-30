eea2d2fc5d748223e27846829002201636ea1871
Stephan Markovski <smarkovski4271@stu.d214.org>
2021-03-30 12:50:12 -0700

    Added IBAction and IBOutlet

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
