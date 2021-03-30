1c8197fe7458499fe77b71ff9b77d7890b18ac2e
Stephan Markovski <smarkovski4271@stu.d214.org>
2021-03-30 13:13:11 -0700

    added tableview cell code

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
     }
 
     @IBAction func whenButtonPressed(_ sender: UIBarButtonItem) {
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
